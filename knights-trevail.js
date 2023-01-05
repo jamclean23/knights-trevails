ace817c63c4d8ee7d32d18a37285383b0404cd54
jamclean23 <jamclean23@gmail.com>
2023-01-05 17:01:02 -0500

    Add links embedded in each node in array, gui

--- knights-trevail.js
+++ knights-trevail.js
@@ -23,12 +23,11 @@ const Board = (function () {
             board.push(row);
         }
 
-        createKnightsTrevails(board);
+        createKnightsTrevails(board, length);
 
         return board;
 
-        function createKnightsTrevails (board) {
-            let length = 0;
+        function createKnightsTrevails (board, length) {
             let potentialMoves = [
                 {x: -1, y: -2},
                 {x: -1, y: 2},
@@ -40,22 +39,39 @@ const Board = (function () {
                 {x: 2, y: 1}
             ];
 
-            board.forEach((element, index) => {
-                ++length;
-            });
-
             board.forEach((row, yIndex) => {
                 row.forEach((square, xIndex) => {
-                    
-                    console.log(square);
+                    potentialMoves.forEach((move) => {
+                        
+                        let potentialMove = validatePotentialMove(xIndex, yIndex, move.x, move.y, length);
+                        if (potentialMove.valid === true) {
+                            square.links.push(board[potentialMove.y][potentialMove.x]);
+                        }
+
+
+                    });
                 });
             });
+
+            function validatePotentialMove (xIndex, yIndex, moveX, moveY, length) {
+                if (((xIndex + moveX >= 0) && (xIndex + moveX < length )) && ((yIndex + moveY >= 0) && (yIndex + moveY < length))) {
+                    return {
+                        x: xIndex + moveX,
+                        y: yIndex + moveY,
+                        valid: true
+                    };
+                }
+                return {
+                    valid: false
+                };
+            }
         }
 
         function createSquare (x, y) {
             return {
                 x,
                 y,
+                links: [],
                 visted: false
             };
         }
@@ -69,4 +85,42 @@ const Board = (function () {
 
 // INIT
 
-Board.createBoard(8);
+let newBoard = Board.createBoard(8);
+
+console.log(newBoard);
+
+// HTML CSS GENERATION
+
+newBoard.reverse().forEach((row, yIndex) => {
+    row.forEach((square, xIndex) => {
+        const board = document.querySelector('.board');
+        let div = document.createElement('div');
+        div.innerText = 'x: ' + square.x + '\ny: ' + square.y;
+        if (yIndex % 2 === 0) {
+            if (!(xIndex % 2 === 0 )) {
+                div.style.color = 'white';
+                div.style.backgroundColor = 'black';
+            }
+        } else {
+            if (xIndex % 2 === 0 ){
+                div.style.color = 'white';
+                div.style.backgroundColor = 'black';
+            }
+        }
+        board.appendChild(div);
+        square.position = div.getBoundingClientRect();
+        let dot = document.createElement('div');
+        dot.style.height = '5px';
+        dot.style.width = '5px';
+        dot.style.position = 'fixed';
+        dot.style.backgroundColor = 'yellow';
+        dot.style.left = square.position.left + square.position.width/2 + 'px';
+        dot.style.top = square.position.top + square.position.height/2 + 'px';
+        
+
+        const html = document.querySelector('html');
+        html.appendChild(dot);
+        console.log(square.position.left);
+
+    });
+});
