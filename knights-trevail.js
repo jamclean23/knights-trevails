f0173d0f2311a648c0295cde11f384f0490b4326
jamclean23 <jamclean23@gmail.com>
2023-01-05 12:56:29 -0500

    Add iife for gameboard, started functions for generating knights graph

--- knights-trevail.js
+++ knights-trevail.js
@@ -2,6 +2,71 @@
 
 // Module for chess board
 
-const chessBoard = (function () {
+const Board = (function () {
     
+    function createNode (xValue, yValue, linkedNode) {
+
+    }
+
+    function createBoard (length) {
+        // create an array of length*length squares
+        // make a loop to account for columns, loop length times
+        // within, make a loop to account for rows loop length times
+        // 
+        let board = [];
+        for (let i = 0; i < length; i++) {
+            let row = [];
+            for (j = 0; j < length; j++) {
+                let square = createSquare(j, i);
+                row.push(square);
+            }
+            board.push(row);
+        }
+
+        createKnightsTrevails(board);
+
+        return board;
+
+        function createKnightsTrevails (board) {
+            let length = 0;
+            let potentialMoves = [
+                {x: -1, y: -2},
+                {x: -1, y: 2},
+                {x: -2, y: -1},
+                {x: -2, y: 1}, 
+                {x: 1, y: -2 }, 
+                {x: 1, y: 2}, 
+                {x: 2, y: -1}, 
+                {x: 2, y: 1}
+            ];
+
+            board.forEach((element, index) => {
+                ++length;
+            });
+
+            board.forEach((row, yIndex) => {
+                row.forEach((square, xIndex) => {
+                    
+                    console.log(square);
+                });
+            });
+        }
+
+        function createSquare (x, y) {
+            return {
+                x,
+                y,
+                visted: false
+            };
+        }
+
+    }
+
+    return {
+        createBoard
+    }
 })();
+
+// INIT
+
+Board.createBoard(8);
