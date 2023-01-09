b9286be8a9eebca470656070906e20431f0f4191
jamclean23 <jamclean23@gmail.com>
2023-01-09 16:51:05 -0500

    Add findRoute(). Functional, untested

--- knights-trevail.js
+++ knights-trevail.js
@@ -3,10 +3,6 @@
 // Module for chess board
 
 const Board = (function () {
-    
-    function createNode (xValue, yValue, linkedNode) {
-
-    }
 
     function createBoard (length) {
         // create an array of length*length squares
@@ -72,55 +68,220 @@ const Board = (function () {
                 x,
                 y,
                 links: [],
-                visted: false
+                ancestry: [],
+                visited: false
             };
         }
 
     }
 
+    function findRoute(currentX, currentY, destinationX, destinationY, board) {
+        console.log(currentX + '-'+ currentY + ',' + destinationX + '-' + destinationY);
+
+        let origin = findSquare(currentX, currentY, board);
+        let destination = findSquare(destinationX, destinationY, board);
+        if (typeof origin === 'object' && typeof destination === 'object') {
+            return breadthSearch(origin, destination);
+        }
+
+        return 'Not Found';
+        
+        // Bread-first search of origin's children for destination
+
+
+        function breadthSearch (origin, destination ) {
+            let queue = [];
+            queue.push(origin);
+            let found = false;
+            let counter = 0;
+            let result = 'Not found';
+
+            while (queue.length && found === false && counter < 1000) {
+                counter++;
+
+                // Dequeue parent
+                let current = queue.shift();
+                current.ancestry.push(current);
+                console.log('current ancestry');
+                console.log(current.ancestry);
+
+                // Check if found
+                if (current === destination) {
+                    result = current;
+                    found = true;
+                    console.log('found');
+                }
+                
+                // Enqueue children
+                if (current.visited === false) {
+                    current.links.forEach((link) => {
+                        if (link.visited === false) {
+                            link.ancestry = [...current.ancestry];
+                            console.log('link');
+                            console.log(link);
+                            queue.push(link);
+                        }
+
+                    });
+                }
+                current.visited = true;
+            }
+            console.log(counter + ' queues');
+            console.log(result);
+            return result.ancestry;
+        }
+
+        function findSquare(x, y, board) {
+            let result = 'Not found';
+            board.forEach((row, yIndex) => {
+                row.forEach((square, xIndex) => {
+                    if (square.x === x && square.y === y) {
+                        result = square;
+                    }
+                });
+            });
+            return result;
+
+        }
+    }
+
     return {
-        createBoard
+        createBoard,
+        findRoute
     }
 })();
+
+
 
 // INIT
 
 let newBoard = Board.createBoard(8);
 
-console.log(newBoard);
-
 // HTML CSS GENERATION
 
-newBoard.reverse().forEach((row, yIndex) => {
-    row.forEach((square, xIndex) => {
-        const board = document.querySelector('.board');
-        let div = document.createElement('div');
-        div.innerText = 'x: ' + square.x + '\ny: ' + square.y;
-        if (yIndex % 2 === 0) {
-            if (!(xIndex % 2 === 0 )) {
-                div.style.color = 'white';
-                div.style.backgroundColor = 'black';
-            }
-        } else {
-            if (xIndex % 2 === 0 ){
-                div.style.color = 'white';
-                div.style.backgroundColor = 'black';
-            }
-        }
-        board.appendChild(div);
-        square.position = div.getBoundingClientRect();
-        let dot = document.createElement('div');
-        dot.style.height = '5px';
-        dot.style.width = '5px';
-        dot.style.position = 'fixed';
-        dot.style.backgroundColor = 'yellow';
-        dot.style.left = square.position.left + square.position.width/2 + 'px';
-        dot.style.top = square.position.top + square.position.height/2 + 'px';
-        
+updateLines();
+getUserInput();
+console.log('---\nBOARD:');
+console.log(newBoard);
 
-        const html = document.querySelector('html');
-        html.appendChild(dot);
-        console.log(square.position.left);
+// FUNCTIONS FOR HTML GENERATION
 
+function getUserInput () {
+    
+   let result = '\n---\nMOVES:' + Board.findRoute(0, 0, 0, 7, newBoard).reduce((string, element) => {
+        return string += ' [' + element.x + ',' + element.y + '] '
+     }, '');
+   console.log(result);
+
+}
+
+function updateLines () {
+
+    const divs = document.querySelectorAll('.square');
+
+
+    // Set Board size in css
+    const root = document.querySelector(':root');
+    root.style.setProperty('--boardSize', newBoard.length);
+    
+    // Find coordinates of squares and update
+    newBoard.reverse().forEach((row, yIndex) => {
+        row.forEach((square, xIndex) => {
+            const board = document.querySelector('.board');
+            let div = document.createElement('div');
+            div.classList.add('square');
+            div.x = square.x;
+            div.y = square.y;
+            div.innerText = 'x: ' + square.x + '\ny: ' + square.y;
+            if (yIndex % 2 === 0) {
+                if (!(xIndex % 2 === 0 )) {
+                    div.style.color = 'white';
+                    div.style.backgroundColor = 'black';
+                }
+            } else {
+                if (xIndex % 2 === 0 ){
+                    div.style.color = 'white';
+                    div.style.backgroundColor = 'black';
+                }
+            }
+            board.appendChild(div);
+            square.divSquare = div;
+            square.divSquare.position = div.getBoundingClientRect();
+    
+        });
     });
-});
+
+    // Remove any existing svgs
+    let svgs = document.querySelectorAll('svg');
+    svgs.forEach((svg) => {
+        svg.remove();
+    });
+
+    // Create Svgs
+    newBoard.forEach((row, yIndex) => {
+        row.forEach((parentSquare, xIndex) => {
+            const html = document.querySelector('html');
+            parentSquare.links.forEach((link, index) => {
+    
+                divs.forEach((linkSquareDiv) => {
+                    if (linkSquareDiv.x == link.x && linkSquareDiv.y == link.y) {
+                        link.divSquare = linkSquareDiv;
+                        
+                    }
+                });
+        
+                let svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+                svg.style.position = 'fixed';
+    
+                let line = document.createElementNS('http://www.w3.org/2000/svg','line');
+                line.setAttribute('style', 'stroke:rgba(255,255,0,.3);stroke-width:4');
+    
+                html.appendChild(svg);
+                svg.appendChild(line);
+    
+                // find the distance between parent node and linked node and set as width of svg
+                let xDistance = Math.abs(parentSquare.divSquare.position.left - link.divSquare.position.left);
+                svg.setAttribute('width', xDistance);
+                let yDistance = Math.abs(parentSquare.divSquare.position.top - link.divSquare.position.top);
+                svg.setAttribute('height', yDistance);
+    
+                // align to the correct node and draw line
+                svg.style.position = 'fixed';
+    
+                let startingX;
+                let startingY;
+                let endingX;
+                let endingY;
+    
+                // x axis
+                if (parentSquare.divSquare.position.left < link.divSquare.position.left) {
+                    startingX = 0;
+                    endingX = xDistance;
+                    svg.style.left = parentSquare.divSquare.position.left + parentSquare.divSquare.position.width/2;
+                } else {
+                    startingX = xDistance;
+                    endingX = 0
+                    svg.style.left = parentSquare.divSquare.position.left + parentSquare.divSquare.position.width/2 - xDistance;
+                }
+    
+                //y axis
+                if (parentSquare.divSquare.position.top < link.divSquare.position.top) {
+                    startingY = 0;
+                    endingY = yDistance;
+                    svg.style.top = parentSquare.divSquare.position.top + parentSquare.divSquare.position.height/2;
+                } else {
+                    startingY = yDistance;
+                    endingY = 0;
+                    svg.style.top = parentSquare.divSquare.position.top + parentSquare.divSquare.position.height/2 - yDistance;  
+                }
+    
+                line.setAttribute('x1', startingX);
+                line.setAttribute('y1', startingY);
+                line.setAttribute('y2', endingY);
+                line.setAttribute('x2', endingX);
+    
+    
+            });
+        });
+    });
+}
